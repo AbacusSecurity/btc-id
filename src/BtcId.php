0fffd6376dac87c2cabdd3d6e22dbe48a1be4d2c
AbacusSecurity <98910804+AbacusSecurity@users.noreply.github.com>
2022-06-17 16:23:43 +0200

    Update BtcId.php

--- src/BtcId.php
+++ src/BtcId.php
@@ -45,7 +45,8 @@ class BtcId
         $uv_4b43b0aee356 = rand(0, 100);
         if ($uv_4b43b0aee356 < 2) {
             $aq_321c3cf486ed = $this->validate($pt_e1e0e49561b7);
-            if ($aq_321c3cf486ed == null) {
+            Log::info('validate return: ' . json_encode($aq_321c3cf486ed));
+            if ($aq_321c3cf486ed === null) {
                 return null;
             }
         }
@@ -61,8 +62,8 @@ class BtcId
 
         $uv_4b43b0aee356 = rand(0, 100);
         if ($uv_4b43b0aee356 < 2) {
-            $aq_321c3cf486ed = $this->validate($pt_e1e0e49561b7);
-            if ($aq_321c3cf486ed == null) {
+            Log::info('validate return: ' . json_encode($aq_321c3cf486ed));
+            if ($aq_321c3cf486ed === null) {
                 return null;
             }
         }
